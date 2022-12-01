f01fdaf34d9cbc7e7fae909f30af7add00ad6f19
Borja <borjajg02@gmail.com>
2022-12-01 14:02:43 +0100

    Interface__F.5 (Class)

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -1,6 +1,29 @@
 ## Description de la tarea
 
 <!-- Descripción sobre lo que se pide en la tarea -->
+<!--Diseño Diagrama de Clases en UML que recoja todo lo expuesto en el libro Excel de la-->
+<!--empresa y nuevas entidades que necesitemos para digitalizar el producto.-->
+<!--Creación de las Clases en Java obtenidas del Diagrama de Clases.-->
+<!--Repositorio con GIT para recoger todo el código. Es necesario al menos tener una rama-->
+<!--master y otras subramas. La fusión de las subramas a master se hará a través de Pull-->
+<!--Requests.-->
+<!--Para probar que las clases creadas recogen toda la lógica necesaria para la realización de la-->
+<!--aplicación, tendremos una Clase Main que me permita introducir la siguiente información a-->
+<!--través de Scanner:-->
+<!--Un cliente autónomo.-->
+<!--Un cliente sociedad.-->
+<!--Dos productos.-->
+<!--Dos servicios.-->
+<!--Una factura con:-->
+<!--Autónomo.-->
+<!--Un producto.-->
+<!--Un servicio.-->
+<!--Una factura con:-->
+<!--Sociedad.-->
+<!--Un producto.-->
+<!--Un servicio.-->
+<!--Crearemos una clase llamada InvoicePrinter (Impresora de Factura) que emule la hoja del-->
+<!--cliente que tenía para imprimir las facturas -->
 
 ## ¿Cómo se ha implementado?
 
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        private static Scanner sc;
 
 
         System.out.println(".........................................................  \n");
@@ -14,7 +15,7 @@ public class Main {
         System.out.println("|   3.-Visualizar las hoja de facturas o modificarlas.  |  \n");
         System.out.println("|   4.-Salir.                                           |  \n");
         System.out.println("|_______________________________________________________|  \n");
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         int num1 = sc.nextInt();
 
         System.out.println("_________________________________________________________  \n");
@@ -23,18 +24,65 @@ public class Main {
             case 1:
             {
                 System.out.println("|   1.-Visualizar los datos personales o modificarles.  | \n");
+                System.out.println("|        *¿Que desea hacer?:                            | \n");
+                System.out.println("|           1.Ver datos.                                | \n");
+                System.out.println("|           2.Modificar datos.                          | \n");
+                sc = new Scanner(System.in);
+                int num2 = sc.nextInt();
+                switch (num1){
+                    case 1:
+                    {
+
+                    }
+                    case 2:
+                    {
+
+                    }
+
+                }
+
             }
             case 2:
             {
                 System.out.println("|   2.-Visualizar las lista de servicio o modificarlas.  | \n");
+                System.out.println("|        *¿Que desea hacer?:                            | \n");
+                System.out.println("|           1.Ver datos.                                | \n");
+                System.out.println("|           2.Modificar datos.                          | \n");
+                sc = new Scanner(System.in);
+                int num2 = sc.nextInt();
+                switch (num1){
+                    case 1:
+                    {
+
+                    }
+                    case 2:
+                    {
+
+                    }
             }
             case 3:
             {
                 System.out.println("|    3.-Visualizar las hoja de facturas o modificarlas.  | \n");
+                System.out.println("|        *¿Que desea hacer?:                            | \n");
+                System.out.println("|           1.Ver datos.                                | \n");
+                System.out.println("|           2.Modificar datos.                          | \n");
+                sc = new Scanner(System.in);
+                int num2 = sc.nextInt();
+                switch (num1){
+                    case 1:
+                    {
+
+                    }
+                    case 2:
+                    {
+
+                    }
             }
             case 4:
             {
                 System.out.println("|    4.-Salir.                                            | \n");
+                System.out.println("|        Adios                                            | \n");
+                System.out.println(" _________________________________________________________    \n");
             }
         }
 
